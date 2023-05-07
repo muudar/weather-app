5b2c9e37624879318af5cd6c65b4bfc3697f8928
Mudhar <mohammedmudher@hotmail.com>
2023-05-07 19:23:56 +0300

    base form and console logs json data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+function getInput(){
+    return document.querySelector("#location").value;
+}
+
+document.querySelector("form").onsubmit = () =>{
+    fetch("https://api.weatherapi.com/v1/forecast.json?key=af8ac87f3a69412f9cb153535230705&q="+getInput()+"&days=7")
+    .then((resp) => {
+        if(resp.ok){
+            return resp.json();
+        }
+            throw new Error("No such location exists!");
+    })
+    .then((resp) => console.log(resp))
+    .catch((error) =>{
+        console.log("Fix dis man!");
+    })
+}
